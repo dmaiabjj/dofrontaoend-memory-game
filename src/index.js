416f418053e02b8cf1b28c57da0a4f6796080bd1
“Diêgo <diegovbarros@gmail.com>
2019-01-14 23:02:31 -0200

    Criando tag pelo JavaScript

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const $root = document.querySelector('#root');
+
+const $memoryCard = document.createElement('article');
+$memoryCard.classList.add('memory-card');
+
+$root.insertBefore($memoryCard, null);
