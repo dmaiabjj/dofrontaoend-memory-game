c2333772e7b8fc069e783d9e154b265937e6c590
Diêgo Maia <diegovbarros@gmail.com>
2019-01-29 12:04:41 -0200

    [027] - Desafio Avançado

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -9,16 +9,26 @@ injectCSS(`
         border-radius: 30px;
         box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
         cursor: pointer;
+        background-color: #f25a70;
+    }
+
+
+    .memory-card>.icon {
+        width: 100px;
+        height: 100px;
+    }
+
+    .memory-card>.icon:first-of-type {
+        display:none;
+    }
+
+    .memory-card>.icon:last-of-type {
+        display:block;
     }
 
     .memory-card.-front {
         background-color: #FFF;
     }
-
-    .memory-card.-back {
-        background-color: #f25a70;
-    }
-
 
     .memory-card.-front::before {
         content: "";
@@ -29,36 +39,56 @@ injectCSS(`
         position: absolute;
     }
 
-
-    .memory-card>.icon {
-        width: 100px;
-        height: 100px;
-    }
-
     .memory-card.-front>.icon {
         position: absolute;
         transform: translateY(-12px);
     }
+
+    .memory-card.-front>.icon:first-of-type {
+        display:block;
+    }
+
+    .memory-card.-front>.icon:last-of-type {
+        display:none;
+    }
 `);
 
+
+// const createMemoryCard = ({
+//     id,
+//     className,
+//     src,
+//     alt,
+// }) => `
+//     <article class="memory-card ${className}">
+//         <img 
+//             class="icon"
+//             src="images/icon-${src}.png" 
+//             alt="${alt}"
+//             onClick="handleClick(${id})"
+//         >
+//     </article>`;
+
+const handleClick = (element) => {
+    element.classList.toggle("-front");
+}
 
 const createMemoryCard = ({
     id,
     className,
     src,
-    alt,
+    alt
 }) => `
-    <article class="memory-card ${className}">
-        <img 
-            class="icon"
-            src="images/icon-${src}.png" 
-            alt="${alt}"
-            onClick="handleClick(${id})"
-        >
-    </article>`;
-
-
-
-function handleClick(position) {
-    console.log(`Card ${position} clicked`);
-}
+        <article class="memory-card ${className}" onClick="handleClick(this)" >
+            <img 
+                class="icon"
+                src="images/icon-${src}.png" 
+                alt="${alt}"
+            >
+            <img 
+                class="icon"
+                src="images/icon-collabcode.png" 
+                alt="Gueio Mascote da Collab"
+            >
+        </article>
+`;
--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,44 +1,59 @@
 const cards = [{
-    id: 1,
-    className: '-front',
-    src: 'js',
-    alt: 'JS - Livro de Javascript'
-}, {
-    id: 2,
-    className: '-front',
-    src: 'js',
-    alt: 'JS - Livro de Javascript'
-}, {
-    id: 3,
-    className: '-front',
-    src: 'c',
-    alt: 'C - Livro de C'
-}, {
-    id: 4,
-    className: '-front',
-    src: 'c',
-    alt: 'C - Livro de C'
-}, {
-    id: 5,
-    className: '-front',
-    src: 'bug',
-    alt: 'Inseto dentro da janela do browser'
-}, {
-    id: 6,
-    className: '-front',
-    src: 'bug',
-    alt: 'Inseto dentro da janela do browser'
-}, {
-    id: 7,
-    className: '-back',
-    src: 'collabcode',
-    alt: 'Gueio - Mascote da CollabCode'
-}, {
-    id: 8,
-    className: '-back',
-    src: 'collabcode',
-    alt: 'Gueio - Mascote da CollabCode'
-}]
+        id: 1,
+        className: '-front',
+        src: 'js',
+        alt: 'JS - Livro de Javascript'
+
+    }, {
+
+        id: 2,
+        className: '-front',
+        src: 'js',
+        alt: 'JS - Livro de Javascript'
+
+    }, {
+
+        id: 3,
+        className: '-front',
+        src: 'c',
+        alt: 'C - Livro de C'
+
+    },
+    {
+        id: 4,
+        className: '-front',
+        src: 'c',
+        alt: 'C - Livro de C'
+    }, {
+
+        id: 5,
+        className: '-front',
+        src: 'bug',
+        alt: 'Inseto dentro da janela do browser'
+
+    }, {
+
+        id: 6,
+        className: '-front',
+        src: 'bug',
+        alt: 'Inseto dentro da janela do browser'
+
+    }, {
+
+        id: 7,
+        className: '-front',
+        src: 'cellphone',
+        alt: 'Telefone celular'
+
+    }, {
+
+        id: 8,
+        className: '-front',
+        src: 'woman',
+        alt: 'Uma mulher em frente ao computador'
+
+    }
+]
 
 const $root = document.querySelector('#root');
 const $cardsWrapper = createCardsWrapper()
