1234619b9790f87006674409d13fc773b29022f9
Diêgo Maia <diegovbarros@gmail.com>
2019-03-13 13:21:55 -0300

    [039] - Desafio Avançado

--- src/objects/gueio-icon/index.js
+++ src/objects/gueio-icon/index.js
@@ -2,6 +2,9 @@ const gueioIcon = (function () {
     injectCSS(`
         .icon {
             width: 171px;
+            margin: 0 auto 0 auto;
+            display: block;
+            transform: translateY(-70px);
         }
     `);
 
